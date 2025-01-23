3b3646c43315ebc1be95676be2a62a43b135cfb0
aissa-dev1 <aissadev1@gmail.com>
2025-01-23 14:06:26 +0100

    include navbar and container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import Container from "@/components/reusable/container";
+import NavBar from "@/components/reusable/nav-bar";
+
 export default function Home() {
   return (
-    <div>
-      <p>Hello world!</p>
-    </div>
+    <>
+      <NavBar />
+      <Container className="pt-[calc(var(--navbar-height)*1.25)]">
+        <p>Hello world!</p>
+      </Container>
+    </>
   );
 }
