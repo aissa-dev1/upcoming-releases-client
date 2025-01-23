9428faa76a8af2ee24485dd3b8701021589d46d2
aissa-dev1 <aissadev1@gmail.com>
2025-01-23 14:05:34 +0100

    create bars3 icon component

---
+++ src/components/icons/outline/bars3.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/utils/cn";
+import { ComponentProps } from "react";
+
+interface Bars3Props extends ComponentProps<"svg"> {}
+
+export default function Bars3Icon({ className, ...rest }: Bars3Props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={cn("size-6", className)}
+      {...rest}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+      />
+    </svg>
+  );
+}
