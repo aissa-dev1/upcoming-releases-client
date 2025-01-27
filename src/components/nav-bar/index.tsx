d1a7f190a6c34651ce92175c5c48d69cecc63dbb
aissa-dev1 <aissadev1@gmail.com>
2025-01-27 22:46:05 +0100

    replace the title normal paragraph element with typography paragraph add a link to redirect to the home page when clicking the title

--- src/components/nav-bar/index.tsx
+++ src/components/nav-bar/index.tsx
@@ -1,12 +1,18 @@
 import Bars3Icon from "@/components/icons/outline/bars3";
 import NavBarSearchContainer from "./search-container";
 import Container from "../container";
+import { P } from "../typography";
+import Link from "next/link";
 
 export default function NavBar() {
   return (
     <nav className="fixed w-full flex items-center bg-foreground text-background h-[var(--navbar-height)]">
       <Container className="flex items-center gap-4">
-        <p className="capitalize shrink-0">Upcoming releases</p>
+        <Link href="/" className="shrink-0">
+          <P size="lg" className="capitalize">
+            Upcoming releases
+          </P>
+        </Link>
         <NavBarSearchContainer />
         <button className="bg-background text-foreground py-1.5 px-2 rounded-md text-sm font-bold uppercase active:scale-95">
           <Bars3Icon />
