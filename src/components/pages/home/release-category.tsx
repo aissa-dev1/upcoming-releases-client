dcd59c9fa062ed3653a25626a20b8a7373712cae
aissa-dev1 <aissadev1@gmail.com>
2025-01-27 22:50:48 +0100

    add id and category props replace what was in the key attribute (random.strongId) with just id prop create a link and a button that redirects to the needed category and its id page

--- src/components/pages/home/release-category.tsx
+++ src/components/pages/home/release-category.tsx
@@ -1,19 +1,24 @@
+import { Button } from "@/components/button";
 import { CardDescription, CardHeader, CardTitle } from "@/components/card";
 import Divider from "@/components/divider";
 import { P } from "@/components/typography";
-import { random } from "@/utils/random";
+import Link from "next/link";
 import React from "react";
 
 export interface ReleaseCategoryProps {
+  id: string;
   title: string;
   description: string;
+  category: string;
   tags?: string[];
   showDivider?: boolean;
 }
 
 export function ReleaseCategory({
+  id,
   title,
   description,
+  category,
   tags,
   showDivider = true,
 }: ReleaseCategoryProps) {
@@ -25,7 +30,7 @@ export function ReleaseCategory({
         {Array.isArray(tags) && (
           <div className="flex gap-1">
             {tags.map((tag, index) => (
-              <React.Fragment key={random.strongId()}>
+              <React.Fragment key={id}>
                 <P size="sm" className="text-gray-foreground capitalize">
                   {tag}
                 </P>
@@ -36,6 +41,9 @@ export function ReleaseCategory({
             ))}
           </div>
         )}
+        <Link href={`/releases/${category}/${id}`} className="w-full sm:w-fit">
+          <Button className="w-full">Visit</Button>
+        </Link>
       </CardHeader>
       {showDivider && <Divider className="w-[calc(100%-1.5rem)]" />}
     </div>
